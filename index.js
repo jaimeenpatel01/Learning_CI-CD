48bbd4294f1c31c84f5222dc73781282d04f79c4
Jaimeen Personal <jaimeenpatel2017@gmail.com>
2025-08-24 08:23:48 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello CI/CD 🚀")
+    res.send("Hello CI/CD 🚀... Learning from scratch !!!")
 })
 
 app.listen(PORT, () => {
