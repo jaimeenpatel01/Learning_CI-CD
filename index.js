d27b0a6f311145547b67476998d0fa11d49c58cb
Jaimeen Personal <jaimeenpatel2017@gmail.com>
2025-08-24 09:14:06 +0530

    made changes in index file

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send("Hello CI/CD 🚀... Learning from scratch !!!")
 })
 
+app.get("/success",(req,res)=>{
+    res.send("Successfully deployed to Render 🚀")
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 })
